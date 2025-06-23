06a7dc0253e638679c867bde3ffec08901c70ef2
bvvivek6 <vivekbv242813@gmail.com>
2025-06-23 14:36:51 +0530

    fix: update MongoDB connection options and format ESLint configuration

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,11 @@
-const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const app = express();
-
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to MongoDB");
   })
